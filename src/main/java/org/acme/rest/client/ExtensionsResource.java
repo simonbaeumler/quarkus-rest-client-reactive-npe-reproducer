22de003101360d9f469bbee8e3da61d97845a7e6
Simon Baeumler <simon.baeumler@qaware.de>
2022-10-29 22:00:56 +0200

    Simplifies setup - Also adds description in README.md

--- src/main/java/org/acme/rest/client/ExtensionsResource.java
+++ src/main/java/org/acme/rest/client/ExtensionsResource.java
@@ -1,14 +1,9 @@
 package org.acme.rest.client;
 
-import io.smallrye.common.annotation.Blocking;
-import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import java.util.Set;
-import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class ExtensionsResource {
@@ -18,6 +13,6 @@ public class ExtensionsResource {
 
 
     public Set<Extension> id(String id) {
-        return extensionsService.getById("token", id);
+        return extensionsService.getById(id);
     }
 }
